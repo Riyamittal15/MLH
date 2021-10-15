4e153f8d062fcdfb59235697bea2aa3125081c81
Riya Mittal <63870023+Riyamittal15@users.noreply.github.com>
2021-10-15 17:56:44 +0530

    Create savingsCalc.py

---
+++ savingsCalc.py
@@ -0,0 +1,11 @@
+print('Your savings account')
+uin= input('How much do you make per day? :')
+
+ds=int(uin)*8
+print("This is your daily savings",ds)
+
+ws=ds*5
+print("This is your weekly savings",ws)
+
+ms=ws*4
+print("This is your monthly savings",ms)
